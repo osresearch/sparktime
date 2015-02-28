22608785c0376bdfe54573b492519721f9f3d91d
Trammell Hudson <hudson@osresearch.net>
2015-02-28 16:12:14 -0500

    output entire row at once

--- sparktime/ledmatrix.ino
+++ sparktime/ledmatrix.ino
@@ -120,7 +120,10 @@ row_output(
 
 	// BSRR has both set and reset functionality, with the bottom
 	// 16-bits for set, and the top 16-bits for clear.
-	gpio_b->BSRR = one_bits | (zero_bits << 16);
+	gpio_b->BSRR = 0
+		| (one_bits << 16)
+		| (zero_bits << 0)
+		;
 }
 	
 
