c0e9bdb94833ea8dad8fc48d1fa1b4d3fbad0385
Trammell Hudson <hudson@osresearch.net>
2015-03-14 13:53:55 -0400

    getting better - timing is closer; some bright pixels

--- redscroller/redscroller.ino
+++ redscroller/redscroller.ino
@@ -1,15 +1,15 @@
 /*
  * 18 * 5x7 modules = 90x7 screen
  * A0 data
- * A1 latch?
- * A2 enable? data2?
+ * A1 enable?
+ * A2 latch?
  * A3 clock1
  * A4 clock2
  */
 
 #define LED_DATA	A0
-#define LED_LATCH	A1
-#define LED_ENABLE	A2
+#define LED_ENABLE	A1
+#define LED_LATCH	A2
 #define LED_CLOCK1	A3
 #define LED_CLOCK2	A4
 
@@ -46,8 +46,8 @@ void setup()
 	{
 		for (int x = 0 ; x < WIDTH ; x++)
 		{
-			//fb[y][x] = (x % 8) > y ? 0 : 1;
-			fb[y][x] = y & 1;
+			fb[y][x] = (x % 8) > y ? 0 : 1;
+			//fb[y][x] = x < WIDTH/2; //(x^y) & 1;
 		}
 	}
 }
@@ -59,41 +59,79 @@ row(
 	const uint8_t * const data
 )
 {
-	digitalWrite(LED_ENABLE, 0);
-
+	// turn on the pin
 	digitalWrite(row_pin, 0);
 	pinMode(row_pin, OUTPUT);
 
-	for (unsigned i = 0 ; i < WIDTH/2 ; i++)
-	{
-		digitalWrite(LED_CLOCK1, 1);
-		digitalWrite(LED_DATA, data[i] ?  1 : 0);
-		digitalWrite(LED_CLOCK1, 0);
-	}
-
-	for (unsigned i = WIDTH/2 ; i < WIDTH ; i++)
-	{
-		digitalWrite(LED_CLOCK2, 1);
-		digitalWrite(LED_DATA, data[i] ?  1 : 0);
-		digitalWrite(LED_CLOCK2, 0);
-	}
-
-	digitalWrite(LED_LATCH, 1);
+	// clock 1 drops first, starting the cycle
+	digitalWrite(LED_CLOCK1, 0);
 	delayMicroseconds(10);
+
+	// a few microseconds later latch goes low
 	digitalWrite(LED_LATCH, 0);
 
-	pinMode(row_pin, INPUT);
+
+	// then the evens are clocked out
+	for (unsigned i = 1 ; i < WIDTH ; i+=2)
+	{
+		digitalWrite(LED_CLOCK1, 1);
+		//delayMicroseconds(1);
+		digitalWrite(LED_DATA, data[i] ?  1 : 0);
+		//delayMicroseconds(1);
+		digitalWrite(LED_CLOCK1, 0);
+		//delayMicroseconds(1);
+	}
+
+	// after the evens clock2 goes HIGH
+	digitalWrite(LED_CLOCK2, 1);
+	delayMicroseconds(1);
+
+	// and then the evens are clocked out
+	for (unsigned i = 0 ; i < WIDTH ; i+=2)
+	{
+		digitalWrite(LED_CLOCK2, 1);
+		//delayMicroseconds(1);
+		digitalWrite(LED_DATA, data[i] ?  1 : 0);
+		//delayMicroseconds(1);
+		digitalWrite(LED_CLOCK2, 0);
+		//delayMicroseconds(1);
+	}
+
+	// always return the data pin to zero
+	digitalWrite(LED_DATA, 0);
+
+
+	// strobe the latch pin to signal that we have finished
+	// clocking out this row.
+	digitalWrite(LED_LATCH, 1);
+	delayMicroseconds(1);
+/*
+	digitalWrite(LED_LATCH, 0);
+	delayMicroseconds(1);
+*/
+
+	// enable goes high a few microseconds before the clocks
 	digitalWrite(LED_ENABLE, 1);
+
+	// reset the clocks to their idle state
+	digitalWrite(LED_CLOCK1, 1);
+	digitalWrite(LED_CLOCK2, 0);
+
+	delayMicroseconds(100);
+
+	// turn off the pin
+	pinMode(row_pin, INPUT);
+	digitalWrite(LED_ENABLE, 0);
 }
 
 
 void loop()
 {
-	row(D0, fb[0]);
-	row(D1, fb[1]);
-	row(D3, fb[2]);
-	row(D4, fb[3]);
-	row(D5, fb[4]);
-	row(D6, fb[5]);
-	row(D7, fb[6]);
+	row(D0, fb[1]);
+	row(D1, fb[2]);
+	row(D3, fb[3]);
+	row(D4, fb[4]);
+	row(D5, fb[5]);
+	row(D6, fb[6]);
+	row(D7, fb[7]);
 }
