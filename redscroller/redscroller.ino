c6d3690f779722c73510107754d402d95db574f2
Trammell Hudson <hudson@osresearch.net>
2015-03-14 12:33:36 -0400

    row outputs work

--- redscroller/redscroller.ino
+++ redscroller/redscroller.ino
@@ -2,41 +2,40 @@ void setup()
 {
 	pinMode(D0, INPUT);
 	pinMode(D1, INPUT);
-	pinMode(D2, INPUT);
 	pinMode(D3, INPUT);
 	pinMode(D4, INPUT);
 	pinMode(D5, INPUT);
 	pinMode(D6, INPUT);
+	pinMode(D7, INPUT);
 
 	digitalWrite(D0, 0);
 	digitalWrite(D1, 0);
-	digitalWrite(D2, 0);
 	digitalWrite(D3, 0);
 	digitalWrite(D4, 0);
 	digitalWrite(D5, 0);
 	digitalWrite(D6, 0);
+	digitalWrite(D7, 0);
 }
+
+static inline void
+row(
+	const int pin
+)
+{
+	pinMode(pin, OUTPUT);
+	digitalWrite(pin, 0);
+	delay(100);
+	pinMode(pin, INPUT);
+}
+
 
 void loop()
 {
-	pinMode(D0, OUTPUT);
-	pinMode(D0, INPUT);
-
-	pinMode(D1, OUTPUT);
-	pinMode(D1, INPUT);
-
-	pinMode(D2, OUTPUT);
-	pinMode(D2, INPUT);
-
-	pinMode(D3, OUTPUT);
-	pinMode(D3, INPUT);
-
-	pinMode(D4, OUTPUT);
-	pinMode(D4, INPUT);
-
-	pinMode(D5, OUTPUT);
-	pinMode(D5, INPUT);
-
-	pinMode(D6, OUTPUT);
-	pinMode(D6, INPUT);
+	row(D0);
+	row(D1);
+	row(D3);
+	row(D4);
+	row(D5);
+	row(D6);
+	row(D7);
 }
